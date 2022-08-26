4fcfb99b0f7c846a81b5245c87ab245e3575a034
Magdalena Łątka <magude6@gmail.com>
2022-08-26 20:37:07 +0200

    Document code

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose');
 const uri = process.env.DB_URI;
 mongoose.connect(uri, { useNewUrlParser: true });
 
+//static content for home, about and contact pages
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -46,6 +47,7 @@ app.get("/compose", (_req, res) => {
   res.render("compose");
 })
 
+//catching post id to render the right object on post page
 app.get("/post/:postId", (req, res) => {
   const requestedPostId = req.params.postId;
 
@@ -56,6 +58,7 @@ app.get("/post/:postId", (req, res) => {
   }) 
 })
 
+//secret page for writing posts and saving them to DB
 app.post("/compose", (req, res) => {
 
   const postTitle = req.body.postTitle;
@@ -70,6 +73,7 @@ app.post("/compose", (req, res) => {
   res.redirect("/");
 })
 
+//post edit rout which redirects to the editing page, had to change form route
 app.post("/post/editpost", (req, res) => {
   console.log(req.body.editButton);
   const postToEditId = req.body.editButton;
@@ -80,6 +84,7 @@ app.post("/post/editpost", (req, res) => {
   })
 })
 
+//edit route where the post gets updated in the DB
 app.post("/edit", (req, res) => {
 
   const postId = req.body.button;
@@ -94,6 +99,7 @@ app.post("/edit", (req, res) => {
   })
 })
 
+//deleting posts from DB
 app.post("/post/deletepost", (req, res) => {
   const postToDeleteId = req.body.deleteButton;
   Post.findByIdAndRemove(postToDeleteId, (err) => {
